ff92b0b128811033b216924ccf328947e963985c
RokasAbra <rokasabramavicius@gmail.com>
2022-04-05 20:49:54 +0300

    vargstam toliau

--- js-uzdaviniai/funkcijos.js
+++ js-uzdaviniai/funkcijos.js
@@ -1,4 +1,5 @@
 //1.Parašyti funkciją, kuri priima du skaičius. Grąžina tų skaičių sumą.
+console.log('------1---------');
 let a = 18;
 let b = 18;
 const suma = (a, b) => a + b   
@@ -11,11 +12,11 @@ console.log('--------2----------');
 function palyginimas(didesnis, lygus) {
     if (a > b) {
         return `Pirmas skaiciu yra didesnis`
-    } else if (a < b) {
+    }if (a < b) {
         return `Antras skacius yra didesnis`
 
         
-    }else if (a = b) {
+    }if (a = b) {
         return `Jie abu yra lygus`
     }
         
@@ -24,10 +25,10 @@ function palyginimas(didesnis, lygus) {
 
  //3.Parašyti funkciją, kuri priimtų vieną skaičių. Turi būti grąžinamas rezultatas ar tie metai yra keliemieji.
 console.log('--------3--------');
-let metai = 1993;
+let metai = 1600;
 function kokieMetai(keliamieji, neKeliamieji) {
     
-    if (metai % 4 === 0 || metai % 400 ===0) {
+    if ((metai % 4 === 0 && metai % 100 !== 0) || metai % 400 === 0) {
         return `Pasirinkti metai ${metai} yra keliamieji`;
     } else return `Pasirinkti metai ${metai} yra ne keliamieji`;
         
@@ -39,39 +40,148 @@ console.log('--------4--------');
 let pirmas = 8;
 const kvadratu = (pirmas) => pirmas * pirmas;
 const rezultatas1 = kvadratu(pirmas);
-console.log(rezultatas1);
+console.log(`Pasirinktas skaicius kvadratu = ${rezultatas1}`);
 
 //5.Parašyti funkciją, kuri priimtų vieną skaičių, kuris turi būti didesnis už vienetą. 
 //Funkcija turi grąžinti visų skaičių sumą, nuo 1 iki įvesto skaičiaus.
-console.log('--------------');
+console.log('-------5-------');
 function skaiciuSuma(sk) {
-    if (sk > 1) {
-         let ats = 0;
-        for (let i = 0; i <= sk; i++) {
-            ats = ats + i;
+  if (sk <= 0) {
+    return 'ERROR ====> Number cant be equal or negative'
+  }
+  let total = 0;
+  for (let i = 0; i < sk; i++) {
+       total += i;
+      
+  }return `Numbers sum: ${total} `
+    
+}console.log(skaiciuSuma(3));
 
-            
-        }return `Skaicius suma: ${ats} iki ivesto skaiciaus: ${sk}`
-    }
-}
- 
- console.log(skaiciuSuma(3));
+const numSum = (n) => n * (n+1) / 2;
+console.log(`Skaiciu suma: ${numSum(3)}`);
+
+  
+  
+  
+  
 
  //6.Parašykite funkciją, kuri priimtų skaičių ir suskaičiuotų, iš kiek sveikų skaičių jos argumentas
  // dalijasi be liekanos (išskyrus vienetą ir patį save).
  console.log('--------6--------');
 
- function sveikuSkaiciu(skc) {
-     for (let i = 0; i < 1000; i++) {
-        
-         if (skc % i === 0) {
-             
-         }return `skaicius dalyjasi is ${skc}, ${sveikuSkaiciu.length}`
+ function sveikuSkaiciu(num) {
+     if (typeof num !== 'number'
+     || !isFinite(num)) {
+        return 'ERROR===> Not a number'
+     }
+
+     if (num ) {
          
      }
-     
- }console.log(sveikuSkaiciu());
+ 
+}console.log(sveikuSkaiciu(true));
 
  console.log('-------------');
 
- 
+
+  function findProperDivisor(num) {
+    if(num<0) return
+      let sum = 0;
+      for (let i = 0; i < Math.floor(num / 2); i++) {
+        if (num % i === 0) {
+          sum += i;
+    console.log(sum);
+        }
+      }
+      return sum
+    }console.log(findProperDivisor(18));
+
+    //7.Parašyti funkciją, kuri priimtų vieną kintamąjį-tekstą. 
+    //Funkcija turi išvesti tekstą į ekraną ir dar papildomai parodyti jo 
+    //ilgį(simbolių kiekį).
+console.log('===========7=========');
+    function text( raides ) {
+        if (typeof raides !== 'string' ) {
+            return `ERROR====> Teksta yra netinkamo tipo`
+        }
+        if (raides === '') {
+            return `ERROR====> Tekstas negali buti tuscias`
+        }
+        let raidziuIlgis = raides.length
+        return  `Tekstas ${raides} sudarytas is: ${raidziuIlgis} simboliu` 
+    }
+console.log(text('asdasd'),);
+console.log(text(1, 2, 3),);
+console.log(text(''),);
+   
+   //8.Parašyti funkcijątelefonoNumeris, kuri priima vieną kintamąjį–masyvą su 10 skaičių jame.
+   //Funkcija turi grąžinti telefono numerį tokiu formatu -"(XXX) XXX-XXXX".
+   console.log('=================8==============');
+   function funkcijątelefonoNumeris(nr) {
+       if (typeof nr !== 'object') {
+           return 'ERROR===> Not a number'
+       }
+       if (nr.length === 0) {
+           return 'ERROR: List cant be empty'
+       }
+       for (let i = 0; i <= nr.length; i++) {
+          if (nr.length == 10 ) {
+              return (`(${nr[0]} ${nr[1]} ${nr[2]}) ${nr[3]} ${nr[4]} ${nr[5]} - ${nr[6]} ${nr[7]} ${nr[8]} ${nr[9]}`);
+          }
+          if (nr.length < 10 ) {
+              return 'ERROR===> Not enough numbers, numbers format: '
+          }
+           
+       }console.log(nr.length);
+   }
+
+console.log(funkcijątelefonoNumeris([8, 5, 6, 8, 9, 7, 8, 9, 3, 1]), '==>', '"(xxx) xxx-xxxx"');
+console.log(funkcijątelefonoNumeris([8, 5, 6, 8, 8, 9, 3, 1]), '==>', '"(xxx) xxx-xxxx"');
+
+//9.Parašyti funkciją, kuri priimtų du kintamuosius-tekstus.Palyginti kuris tekstas yra ilgesnis.
+
+console.log('==============9===========');
+function duKintamieji(pirmas, antras) {
+   
+    if (pirmas.length > antras.length) {
+        return `Pirmas zodis yra is: ${pirmas.length} raidziu ir didesnis uz antra kurio dydis: ${antras.length}`
+    }
+    if (antras.length > pirmas.length) {
+        return `Antras zodis yra is: ${antras.length} raidziu ir didesnis uz pirma kurio dydis: ${pirmas.length}`
+    }
+    if (pirmas.length >= antras.length) {
+        return `Jie abu yra lygus`
+    }
+}
+
+console.log(duKintamieji('pirmasa', 'antras'));
+
+//10.Parašyti funkciją, kuri priimtų vieną kintamąjį-tekstą.Suskaičiuoti, kiek tekste yra ‚a‘ raidžių.
+console.log('////////////////10\\\\\\\\\\\\\\\\\\\\\\\\\\');
+
+function kintamasisA(text) {
+    if (typeof text !== 'string') {
+        return 'ERROR===> Wrong type'
+    }
+    
+   for (let i = 0; i < text.length; i++) {
+       let count = 0;
+           if (text[i] == 'a') {
+               count++;
+           }return 
+         {
+               
+           console.log(`${text[i]} occurs ${count} times`);
+           
+       }
+           
+       }return text
+       
+}
+
+console.log(kintamasisA('assada'), `a tekste,`);
+
+
+
+//if(obj[str[i]]){ obj[str[i]] += 1; }
+
