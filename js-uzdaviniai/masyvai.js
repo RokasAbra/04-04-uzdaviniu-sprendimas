2f55f41bdbfc8b772118c9004c5a33e38f4bc630
RokasAbra <rokasabramavicius@gmail.com>
2022-04-07 21:05:35 +0300

    3.uzd Done

--- js-uzdaviniai/masyvai.js
+++ js-uzdaviniai/masyvai.js
@@ -16,10 +16,29 @@ kurio reikšmės būtų skaičiai nuo 1 iki masyvo ilgio.
 */
 console.log("============2=============");
 
-var myArray = [];
-var arrayMax = Math.floor(Math.random() * (60 - 40 + 1)) + 40;
-var limit = arrayMax;
+let myArray = [];
+let arrayMax = Math.floor(Math.random() * (60 - 40 + 1)) + 40;
+let limit = arrayMax;
 for (var i = 0; i < arrayMax; i++) {
   myArray.push(Math.floor(Math.random() * limit));
 }
 console.log(myArray, myArray.length);
+
+/*
+3.Sukurti atsitiktinio ilgio masyva, kurio ilgisnuo 10iki 15, o 
+reikšmės –atsitiktiniai skaičiai nuo 5 iki 47.Surasti didžiausia skaičių masyve,
+ nenaudojant sort ir Math.max funkcijų.
+*/
+console.log("=============3===========");
+
+let mas = [];
+let arrayMax1 = Math.floor(Math.random() * (15 - 10 + 1) + 10);
+let limit1 = Math.floor(Math.random() * (47 - 5 + 1) + 5);
+let largest = 0;
+for (let i = 0; i <= arrayMax1; i++) {
+  mas.push(Math.floor(Math.random() * limit1));
+  if (mas[i] > largest) {
+    largest = mas[i];
+  }
+}
+console.log(mas, `Didziausia skaicius :`, largest);
